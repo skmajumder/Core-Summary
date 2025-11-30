f3ead08b09b947c560840e86fb16bfda6c4e09c9
skmajumder <skmajumder.cse@gmail.com>
2025-11-30 17:20:25 +0600

    Test shadcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const handleClick = () => {
+    console.log("Button clicked");
+  };
+  
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-200 font-sans">
-      <h1>Core Summary</h1>
+      <Button className="cursor-pointer" onClick={handleClick}>Click me</Button>
     </div>
   );
 }
