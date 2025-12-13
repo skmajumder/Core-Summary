51ba148d496026c90e9988d9a5147d1012c0adde
skmajumder <skmajumder.cse@gmail.com>
2025-12-13 18:06:09 +0600

    Update NextJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  const handleClick = () => {
-    console.log("Button clicked");
-  };
-  
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-200 font-sans">
-      <Button className="cursor-pointer" onClick={handleClick}>Click me</Button>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-200 font-sans container">
+      <h1>Core Summary</h1>
     </div>
   );
 }
